20f0f2ad5f432f2585ccdba6ac60d0ecf36bf394
nushigo <35212723+nushigo@users.noreply.github.com>
2020-10-15 22:31:26 +0900

    Create content_def.js

---
+++ tli/common/js/content_def.js
@@ -0,0 +1,22 @@
+/**
+Name:content default JS
+Version:0.5
+URL:https://nushigo.github.io/tli/common/js/content_def.js
+Last Update:2016/07/21
+**/
+
+function def_header(){
+document.write('<header>\
+<a target="_top" href="https://nushigo.github.io/tli/"><img src="https://nushigo.github.io/tli/common/img/header.jpg" width="1000px" height="200px" alt="TEC Laboratory Introduction"></a>\
+</header>\
+');}
+
+function def_footer(){
+document.write('<footer>\
+<div class="ft_content">\
+<nav><p><a target="_top" href="https://nushigo.github.io/tli/">トップページ</a> | <a target="_top" href="https://nushigo.github.io/tli/search/place.html">実習室別</a> | <a target="_top" href="https://nushigo.github.io/tli/search/maker.html">メーカー別</a> | <a target="_top" href="https://nushigo.github.io/tli/search/type.html">タイプ別</a> | <a target="_top" href="https://nushigo.github.io/tli/search/os.html">OS別</a> | <a target="_top" href="https://nushigo.github.io/tli/pepper/">Pepper（てっくん）</a></p></nav>\
+<p><center>&copy; 2016 Moshinuku!!</center></p>\
+<div align="right" oncontextmenu="return false;"><a href="#top">▲ページの先頭に戻る</a></div>\
+</div>\
+</footer>\
+');}
