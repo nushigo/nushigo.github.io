5f2bd8c6aab37520e10eb146c6a9fbb1712e5de1
nushigo <amatiamariomail+github@gmail.com>
2020-10-16 22:41:40 +0900

    Update File

--- tli/common/js/def.js
+++ tli/common/js/def.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
 Name:Default Script
 URL:https://nushigo.github.io/tli/common/js/def.js
@@ -17,25 +16,4 @@ $(function(){
 		$("html, body").animate({scrollTop:position}, speed, "swing");
 		return false;
 	});
-});	
-=======
-/**
-Name:Default Script
-URL:https://nushigo.github.io/tli/common/js/def.js
-Author:MoshiNuku!!(Wa,Ki,Yo,Nu)
-Last Update:2016/07/21
-**/
-
-/* ページ先端へスクロール */
-/** #がつく所にページ内なら何処でも移動するよ、キミは何処へ行く？ **/
-$(function(){
-	$('a[href^=#]').click(function(){
-		var speed = 1000;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
-});	
->>>>>>> 882764e13b15be3d2b6dbe781f764d1fc2db20d7
+});
