137f23a424ed9ff12bc0ff84d24fd14090b42255
nushigo <amatiamariomail+github@gmail.com>
2020-10-17 15:56:28 +0900

    Upload File

---
+++ jzm/common/js/content_def.js
@@ -0,0 +1,9 @@
+/**
+name: Content Default JS
+version:0.1
+Last Update: 2017/10/25
+**/
+
+function def_blogstoolbar_scl_phtbl(){
+	document.write('');
+}
---
+++ jzm/common/js/def.js
@@ -0,0 +1,15 @@
+/**
+name: Default JS
+version:0.1
+Last Update: 2017/10/28
+**/
+
+/*  jquery.sticky.js用 */
+/** スクロールする際に固定したい要素を指定する **/
+
+$(document).ready(function(){
+	$("#main-head").sticky({topSpacing:0,center: true,});
+});
+
+
+
